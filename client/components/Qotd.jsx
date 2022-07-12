53bd0b0736b53826bf73a771aaeda5792e499c20
Elliot Adinolfi <eadinolfi@Elliots-MBP.lan>
2022-07-11 23:33:39 -0400

    added functionality to grab question from leetcode

--- client/components/Qotd.jsx
+++ client/components/Qotd.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AceEditor from "react-ace";
-
+const axios = require('axios');
 // possible languages
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/mode-typescript";
@@ -21,14 +21,13 @@ import "ace-builds/src-noconflict/theme-tomorrow";
 import "ace-builds/src-noconflict/theme-github";
 
 import "ace-builds/src-noconflict/ext-language_tools";
+import { constantOtherSymbol } from 'ace-builds/src-noconflict/mode-ruby';
 
 
-const Qotd = () => {
+
+const Qotd = props => {
   const [theme, setTheme] = useState('twilight');
-  const [solution, setSolution] = useState(`const twoSum = (array, target) => {
-
-}`);
-
+  
   const changeTheme = (e) => {
     setTheme(e.target.textContent);
   };
@@ -41,15 +40,14 @@ const Qotd = () => {
     alert(solution);
   }
 
-  const problem = "<p>You are given an integer array <code>nums</code>. You are initially positioned at the array&#39;s <strong>first index</strong>, and each element in the array represents your maximum jump length at that position.</p>\n\n<p>Return <code>true</code><em> if you can reach the last index, or </em><code>false</code><em> otherwise</em>.</p>\n\n<p>&nbsp;</p>\n<p><strong>Example 1:</strong></p>\n\n<pre>\n<strong>Input:</strong> nums = [2,3,1,1,4]\n<strong>Output:</strong> true\n<strong>Explanation:</strong> Jump 1 step from index 0 to 1, then 3 steps to the last index.\n</pre>\n\n<p><strong>Example 2:</strong></p>\n\n<pre>\n<strong>Input:</strong> nums = [3,2,1,0,4]\n<strong>Output:</strong> false\n<strong>Explanation:</strong> You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.\n</pre>\n\n<p>&nbsp;</p>\n<p><strong>Constraints:</strong></p>\n\n<ul>\n\t<li><code>1 &lt;= nums.length &lt;= 10<sup>4</sup></code></li>\n\t<li><code>0 &lt;= nums[i] &lt;= 10<sup>5</sup></code></li>\n</ul>\n";
-
-
-
   return (
     <div id="qotd">
       <div id="prompt">
-        <h1 id="question">QOTD: Two-Sum</h1>
-        <p id="problem" dangerouslySetInnerHTML={{__html: problem}}></p>
+        <h1 id="question">QOTD: {props.title}</h1>
+        <div className="problem">
+          <p>Difficulty: {props.difficulty}</p>
+          <p id="problem" dangerouslySetInnerHTML={{__html: props.problem}}></p>
+        </div>
       </div>
       <div id="userInput">
         <h1>Input Your Solution</h1>
@@ -72,7 +70,7 @@ const Qotd = () => {
           showPrintMargin={true}
           showGutter={true}
           highlightActiveLine={true}
-          value={solution}
+          value={props.solution}
           onChange={handleChange}
           setOptions={{
             showLineNumbers: true,
