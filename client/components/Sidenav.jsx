43a06d9248f72be60ccfe32c03d751babfa21e2b
CodedMarc <doramm4408@gmail.com>
2022-07-13 15:58:54 -0500

    Finished thing

--- client/components/Sidenav.jsx
+++ client/components/Sidenav.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Sidenav = () => {
   return (
     <nav>
-      <h1>33DIFF</h1>
+      <h1 id="Logo">CheetCode</h1>
       <Link to="/">Home</Link>
       <Link to="/solutions">Solutions</Link>
     </nav>
