a3b34870b216f50cbc190f3ce40544b89bcd979b
Adam Rodriguez <adamxrodriguez@gmail.com>
2022-07-12 21:09:42 -0400

    db link with table

--- server/models/qModel.js
+++ server/models/qModel.js
@@ -1,6 +1,10 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+
+const PG_URI = 'postgres://blmcsufe:LuBfrjSIMlhGYBME5gWQfw9T8dhQOUhr@chunee.db.elephantsql.com/blmcsufe';
+// SELECT * FROM "public"."persisted_neet_75_tables_problems_table_list_csv" 
+
 const pool = new Pool({
   connectionString: process.env.PG_URI
 });
